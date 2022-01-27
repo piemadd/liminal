02c70b5f62926006c1b33bccb97164046610089b
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-01-27 23:10:33 +0000

    lmao fixed

--- build.js
+++ build.js
@@ -3,3 +3,9 @@ const fs = require('fs');
 filenames = fs.readdirSync('./pics');
 
 fs.writeFileSync('list.json', JSON.stringify(filenames))
+
+const outHtml = '<link rel="prefetch" href="./pics/' + filenames.join('" />\n<link rel="prefetch" href="./pics/') + '" />'
+
+fs.writeFileSync('prefetch.html', outHtml)
+
+//<link rel="prefetch" href="http://example.com/image.png" />
